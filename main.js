3024ba141ecc8de4489c5389e3d3f9ab3e2371cc
israa fathi <israafathi34@gmail.com>
2023-07-12 18:45:06 +0200

    edit

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
 var array = [
     {
-         quote: 'Oh yes, the past can hurt. But the way I see it,you can either run from it or learn from it.',
-         author: '—Rafiki, The Lion King'},
+        quote: 'Oh yes, the past can hurt. But the way I see it,you can either run from it or learn from it.',
+        author: '—Rafiki, The Lion King'},
 
-    {
-        author: '—Fairy Godmother, Cinderella', 
-        quote: 'Even miracles take a little time.'
+    { 
+        quote: 'Even miracles take a little time.',
+        author: '—Fairy Godmother, Cinderella'
+    },
+    { 
+        quote: 'The flower that blooms in adversity is the most rare and beautiful of all.',
+        author: '—The Emperor, Mulan'
     },
     {
-        author: '—The Emperor, Mulan', 
-        quote: 'The flower that blooms in adversity is the most rare and beautiful of all.'
+        quote: 'Venture outside your comfort zone. The rewards are worth it.',
+        author: '—Rapunzel'
     },
     {
-        author: '—Rapunzel', 
-        quote: 'Venture outside your comfort zone. The rewards are worth it.'
-    },
-    {
-        author: '—Piglet, Winnie the Pooh ', 
-        quote: 'The things that make me different are the things that make me.'
-    },
+        quote: 'The things that make me different are the things that make me.',
+        author: '—Piglet, Winnie the Pooh '
+
+    }
 
     
 
 ];
+
+
 
 function randomQuote() {
     var random = Math.floor(Math.random() * array.length);
@@ -33,6 +36,4 @@ function randomQuote() {
 
     
   }
-  
-  document.querySelector('#quoteButton').onclick = randomQuote;
   
